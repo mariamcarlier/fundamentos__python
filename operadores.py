f72a06fc4ba88bd3e63f125cecd358f22c4b8978
mariam00estudio-creator <mariam00estudio@gmail.com>
2026-04-29 12:39:41 -0500

    modificaciones

--- operadores.py
+++ operadores.py
@@ -7,6 +7,26 @@ valor2 = float (input("Ingrese el segundo número: "))
 tipo_operacion = int (input("Ingrese la operación que desea realizar "
 "(1. suma, 2. resta, 3. multiplicación, 4. división): "))
 
+if tipo_operacion == 1:
+    resultado = valor1 + valor2
+    print(f"Suma: {resultado}")
+
+if tipo_operacion == 2:
+    resultado = valor1 - valor2
+    print(f"Resta: {resultado}")
+
+if tipo_operacion == 3:
+    resultado = valor1 * valor2
+    print(f"Multiplicación: {resultado}")
+
+if tipo_operacion == 4:
+    if valor2 != 0:
+        resultado = valor1 / valor2
+        print(f"División: {resultado}")
+    else:
+        print("Error: No se puede dividir por cero.")
+
+
 suma = valor1 + valor2
 resta = valor1 - valor2
 multiplicacion = valor1 * valor2    
@@ -32,4 +52,4 @@ print(f"Exponenciación:{exponenciacion}")
 opcion1 = int (input("Ingrese el primer número: "))
 opcion2 = int (input("Ingrese el segundo número: "))
 
-
+print( tipo_operacion)
